4fa5c3fc8e89c51bb29b8f5d70de86ede528546e
AdrianBurgosA <aaburgos1@espe.edu.ec>
2022-02-19 20:45:16 -0500

    Agregado los formularios de registrar estudiante y periodo

--- 2. Código/Codigo_v1.0/frontend/src/App.js
+++ 2. Código/Codigo_v1.0/frontend/src/App.js
@@ -2,20 +2,19 @@ import React from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import ImportData from './pages/ImportData';
 import ImportProfessors from './pages/ImportProfessors';
+import RegisterStudent from './pages/RegisterStudent'
+import RegisterPeriod from './pages/CreatePeriod'
 
 const App = () => {
-
   return (
-  
     <BrowserRouter>
-
       <Routes>
         <Route path="/importData" element={<ImportData />} />
         <Route path="/importProfessors" element={<ImportProfessors />} />
-
+        <Route path="/registerStudent" element={<RegisterStudent/>}/>
+        <Route path="/registerPeriod" element={<RegisterPeriod/>}/>
       </Routes>
     </BrowserRouter>
-    
   )
 }
 
---
+++ 2. Código/Codigo_v1.0/frontend/src/components/RegisterStudentForm.js
@@ -0,0 +1,63 @@
+import React,{useState} from 'react'
+import Box from '@mui/material/Box'
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import AdapterDateFns from '@mui/lab/AdapterDateFns';
+import DesktopDatePicker from '@mui/lab/DesktopDatePicker';
+import LocalizationProvider from '@mui/lab/LocalizationProvider';
+import Typography from '@mui/material/Typography';
+
+const RegisterStudentForm = () => {
+    const [ student , setStudent ] = useState({
+        name: '',
+        lastName: '',
+        bornYear: new Date(),
+        idCard:'',
+        genre: '',
+        nationality: '',
+        email: '',
+        user: '',
+        password: '',
+    })
+
+    return(
+        <LocalizationProvider dateAdapter={AdapterDateFns}>
+            <br/><Typography variant="h3" gutterBottom component="div" sx={{textAlign:'center'}}>
+                Registrar nuevo estudiante
+            </Typography><br/><br/>
+            <Box
+                component="form"
+                sx={{
+                    '& > :not(style)': { m: 1, width: '80ch'},
+                }}
+            >
+                <TextField value={student.name} onChange={(event) => setStudent({...student,name:event.target.value})}id="outlined-basic" label="Nombre" variant="outlined" /><br/>
+                <TextField value={student.lastName} onChange={(event) => setStudent({...student,lastName:event.target.value})}id="outlined-basic" label="Apellido" variant="outlined"/><br/>
+                <TextField value={student.idCard} onChange={(event) => setStudent({...student,idCard:event.target.value})}id="outlined-basic" label="Cédula" variant="outlined" /><br/>
+                <TextField value={student.genre} onChange={(event) => setStudent({...student,genre:event.target.value})}id="outlined-basic" label="Género" variant="outlined" /><br/>
+                <TextField value={student.nationality} onChange={(event) => setStudent({...student,nationality:event.target.value})}id="outlined-basic" label="Nacionalidad" variant="outlined"/><br/>
+                <TextField value={student.email} onChange={(event) => setStudent({...student,email:event.target.value})}id="outlined-basic" label="Correo eléctronico" variant="outlined" /><br/>
+                <TextField value={student.user} onChange={(event) => setStudent({...student,user:event.target.value})}id="outlined-basic" label="Usuario" variant="outlined" /><br/>
+                <TextField value={student.password} onChange={(event) => setStudent({...student,password:event.target.value})}id="outlined-basic" label="Contraseña" variant="outlined" /><br/>
+                <DesktopDatePicker
+                    label="Date desktop"
+                    inputFormat="MM/dd/yyyy"
+                    renderInput={(params) => <TextField {...params} />}
+                /><br/>
+                <Button
+                    variant="contained"
+                    size = "large"
+                    type="submit"
+                    sx={{
+                        boxShadow: '1px 1px 5px #333',
+                        background: 'linear-gradient(to right, #4d83b8, #4d83b8)',
+                    }}        
+                >
+                    Registrar
+                </Button><br/>
+            </Box>
+        </LocalizationProvider>
+    );
+}
+
+export default RegisterStudentForm;
